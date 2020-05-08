dc40afbc689d7e3e927d2c062487cdf4df1ddbc9
Tate Capra <tatecapra@outlook.com>
2020-05-08 18:20:04 -0500

    added contact button still working on info

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
 // Function to clear the body and replace with contact info
+//button added to clear text and all div
+var newContactScreen;
 
-function clearBody(elementId){
-  
-  document.getElementById(elementId).innerHTML = "";
+function clearBody(elementID){
+    console.log("TEST Test");
+    document.getElementById(elementID).innerHTML = newContactScreen;
 
 }
+
+newContactScreen = "<div>Hello World</div>"
