35014dbc2ea50b72567a26b6cc40f20c62ac3f85
Tate Capra <tatecapra@outlook.com>
2020-05-08 18:33:01 -0500

    home button now takes you back to the main page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 // Function to clear the body and replace with contact info
 //button added to clear text and all div
+
+// var to store new page
 var newContactScreen;
 
 function clearBody(elementID){
@@ -8,4 +10,27 @@ function clearBody(elementID){
 
 }
 
-newContactScreen = "<div>Hello World</div>"
+newContactScreen = `
+<!-- Header -->
+  <div class='row'>
+    <div class='col-12'>
+      <!-- second level of row for name and nav bar -->
+      <div class='row'>
+          <div class='col-12'>
+            <!-- Bootstrap for header running full body width -->
+            <div class='jumbotron jumbotron-fluid text-center'>
+                <div class='container'>
+                  <h1 class='display-4'>Tate Capra</h1>
+                  <p class='lead'>Jr Java & Full Stack Developer</p>
+                  <!-- circle image of profile -->
+                  <img src='assets/photo/tate1.jpg' class='rounded-circle mx-auto image1' alt='profile image'/>
+                  <!-- space for contact button -->
+                  </br></br>
+                  <!-- button for Home -->
+                  <input type='button' value='Home Page'
+                  onclick='window.location.href="https://twcap.github.io/Profile/"'/>
+                </div>
+              </div>
+          </div>
+      </div>
+`;
