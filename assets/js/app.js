d556ebd8e5e701367a0ad1fabfe6f992abed3270
Tate Capra <tatecapra@outlook.com>
2020-05-22 16:46:40 -0500

    Changes to the app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,25 +12,26 @@ function clearBody(elementID){
 
 newContactScreen = `
 <!-- Header -->
-  <div class='row'>
-    <div class='col-12'>
-      <!-- second level of row for name and nav bar -->
-      <div class='row'>
-          <div class='col-12'>
-            <!-- Bootstrap for header running full body width -->
-            <div class='jumbotron jumbotron-fluid text-center'>
-                <div class='container'>
-                  <h1 class='display-4'>Tate Capra</h1>
-                  <p class='lead'>Jr Java & Full Stack Developer</p>
-                  <!-- circle image of profile -->
-                  <img src='assets/photo/tate1.jpg' class='rounded-circle mx-auto image1' alt='profile image'/>
-                  <!-- space for contact button -->
-                  </br></br>
-                  <!-- button for Home -->
-                  <input type='button' value='Home Page'
-                  onclick='window.location.href="https://twcap.github.io/Profile/"'/>
-                </div>
+<div class="row">
+  <div class="col-12">
+    <!-- second level of row for name and nav bar -->
+    <div class="row">
+      <!-- ************************************************** -->
+        <div class="col-12">
+          <!-- Bootstrap for header running full body width -->
+          <div class="jumbotron jumbotron-fluid text-center">
+              <div class="container extraColor">
+                <h1 class="display-4">Tate Capra</h1>
+                <p class="lead">Full Stack Web Developer & Software Engineer</p>
+                <!-- circle image of profile -->
+                <img src="assets/photo/tate1.jpg" class="rounded-circle mx-auto image1" alt="profile image"/>
+                <!-- space for contact button -->
+                </br></br>
+                <input type='button' value='Home Page'onclick='window.location.href="https://twcap.github.io/Profile/"'/>
               </div>
+            </div>
+        </div>
+    </div>
           </div>
       </div>
 
